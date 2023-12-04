c25dd79425e43cbefbbf91fb04dec1e706de89d8
Gabriel Durante <gabrielhdurante@iclou.com>
2023-12-04 16:59:29 -0300

    tudo certo

---
+++ apiCall.js
@@ -0,0 +1,12 @@
+let requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+  
+  const apiCall = fetch("api.coincap.io/v2/assets/bitcoin", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+  
+  console.log(apiCall);
+  
